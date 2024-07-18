c2fe5548e81c24d03d32770a6f74ab8139f34164
isai7710 <isai10sanchez@gmail.com>
2024-07-18 11:16:44 -0600

    implemented blog cards component into my blog route

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,11 @@
-import { getSortedPostsData } from "@/app/lib/posts.js";
+import BlogCards from "@/app/components/blog-cards";
 
 export default async function Gallery() {
-  const allPostsData = await getSortedPostsData();
   return (
     <section className="flex flex-col items-center text-palette-3">
-      <h2 className="text-3xl p-3">Blog Posts</h2>
-      <ul className="grid grid-cols-3 gap-3">
-        {allPostsData.map(({ id, date, title }) => (
-          <li className="" key={id}>
-            {title}
-            <br />
-            {id}
-            <br />
-            {date}
-          </li>
-        ))}
-      </ul>
+      <h2 className="text-3xl p-2">Blog Posts</h2>
+      <p className="mb-2 italic">Welcome to my blog posts!</p>
+      <BlogCards />
     </section>
   );
 }
