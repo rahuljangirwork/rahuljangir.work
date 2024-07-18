e974c2e5ef3ceac18681f7c3840ff29771b1be43
isai7710 <isai10sanchez@gmail.com>
2024-07-18 11:19:17 -0600

    title looks better

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import BlogCards from "@/app/components/blog-cards";
 export default async function Gallery() {
   return (
     <section className="flex flex-col items-center text-palette-3">
-      <h2 className="text-3xl p-2">Blog Posts</h2>
+      <h2 className="text-3xl m-1">Blog Posts</h2>
       <p className="mb-2 italic">Welcome to my blog posts!</p>
       <BlogCards />
     </section>
