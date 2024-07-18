f0c3a95fd61f85a7857b509b5cfd87ec01dcb59d
isai7710 <isai10sanchez@gmail.com>
2024-07-18 11:15:13 -0600

    added my implementation of the shadcn card component into a larger cards component for my blog posts

---
+++ app/components/blog-cards.tsx
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import { getSortedPostsData } from "@/app/lib/posts";
+
+export default async function BlogCards() {
+  const allPostsData = await getSortedPostsData();
+  return (
+    <div className="w-full max-w-4xl mx-auto px-4">
+      <ul className="grid grid-cols-2 md:grid-cols-3 gap-3">
+        {allPostsData.map(({ id, date, title, src }) => (
+          <li key={id} className="h-full">
+            <Card src={src} className="flex flex-col h-full">
+              <CardHeader className="flex-grow">
+                <CardTitle className="">{title}</CardTitle>
+                <CardDescription className="">{id}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <p>{date}</p>
+              </CardContent>
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
