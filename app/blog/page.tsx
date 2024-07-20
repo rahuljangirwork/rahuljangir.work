4bd24da7bbd73ee92665b108fb1efa0e698d1ea1
isai7710 <isai10sanchez@gmail.com>
2024-07-20 10:28:58 -0600

    fixed issue with a quotation mark in a paragraph element by just putting the string into a variable

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,19 +1,14 @@
 import BlogCards from "@/app/components/blog-cards";
 
-export default async function Gallery() {
+export default async function Blog() {
+  const intro = "I always wanted an online site to write about the things I found most interesting about the mechanical engineering field, and about life too. Here you can read about me babbling about everything from a how I set up my Neovim configuration to the way the Heat Equation was derived and everything in between. I'd love to hear your feedback or have a discussion about any of these topics, hit me up!"
   return (
     <>
       <div className="flex flex-col items-center text-palette-3">
         <h2 className="text-3xl m-1">Blog Posts</h2>
         <p className="mb-2 italic">Welcome to my blog posts!</p>
       </div>
-      <p className="w-full text-palette-3 bold">
-        I always wanted an online site to write about the things I found most
-        interesting about the mechanical engineering field, and about life too.
-        Here you can read about me babbling about everything from a how I set up
-        my Neovim configuration to the way the Heat Equation was derived and
-        everything in between. I'd love to hear your feedback or have a
-        discussion about any of these topics, hit me up!
+      <p className="w-full text-palette-3 bold">{intro}
       </p>
       <BlogCards />
     </>
