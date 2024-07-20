2cbbd63f0cb91c2ffadac14968dfb3e5970bb7ee
isai7710 <isai10sanchez@gmail.com>
2024-07-20 10:16:21 -0600

    cards only have the title and description atm

--- app/components/blog-cards.tsx
+++ app/components/blog-cards.tsx
@@ -11,7 +11,7 @@ import { getSortedPostsData } from "@/app/lib/posts";
 export default async function BlogCards() {
   const allPostsData = await getSortedPostsData();
   return (
-    <div className="w-full max-w-4xl mx-auto px-4">
+    <div className="w-full max-w-4xl mx-auto px-4 text-palette-3">
       <ul className="grid grid-cols-2 md:grid-cols-3 gap-3">
         {allPostsData.map(({ id, date, title, src }) => (
           <li key={id} className="h-full">
