ab58c0c87a888d4fe5e8bd444a87dc02d0dadc8a
isai7710 <isai10sanchez@gmail.com>
2024-07-22 14:51:59 -0600

    structuring page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,15 +1,29 @@
 import BlogCards from "@/app/components/blog-cards";
+import { Separator } from "@/app/components/ui/separator";
+import { PenTool } from "lucide-react";
 
 export default async function Blog() {
-  const intro = "I always wanted an online site to write about the things I found most interesting about the mechanical engineering field, and about life too. Here you can read about me babbling about everything from a how I set up my Neovim configuration to the way the Heat Equation was derived and everything in between. I'd love to hear your feedback or have a discussion about any of these topics, hit me up!"
+  const intro =
+    "I always wanted an online space to share my thoughts on the topics I found most captivating about the mechanical engineering field, and about life too. Here, you'll find my reflections and ramblings on everything from how I set up my Neovim configuration to the intricate way the Heat Equation was derived and everything in between. I'd love to hear your feedback or have a discussion about any of these topics, feel free to reach out!";
   return (
     <>
-      <div className="flex flex-col items-center text-palette-3">
-        <h2 className="text-3xl m-1">Blog Posts</h2>
-        <p className="mb-2 italic">Welcome to my blog posts!</p>
+      <div className="flex flex-col items-center w-ful max-w-5xl mx-auto text-palette-3">
+        <h2 className="flex items-center text-3xl py-2 gap-2">
+          <PenTool />
+          Blog Posts
+        </h2>
+        <Separator className="my-2 bg-palette-1" />
+        <div className="flex items-center space-x-8 py-3">
+          <ul>
+            <h3>Contents</h3>
+            <li>One</li>
+            <li>Two</li>
+            <li>Three</li>
+          </ul>
+          <Separator orientation="vertical" className="h-[80px] bg-palette-1" />
+          <p className="w-full text-palette-3 bold py-2">{intro}</p>
+        </div>
       </div>
-      <p className="w-full text-palette-3 bold">{intro}
-      </p>
       <BlogCards />
     </>
   );
