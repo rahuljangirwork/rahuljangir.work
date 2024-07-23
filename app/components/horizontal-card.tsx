4c96b2dcde7df15ae228e8d9a338926aed6dc138
isai7710 <isai10sanchez@gmail.com>
2024-07-23 12:52:25 -0600

    implemented a more destructured approach to building the card component based on the prop 'order' which defines the order of content the card displays, learned more typescript interface and type definition here

--- app/components/horizontal-card.tsx
+++ app/components/horizontal-card.tsx
@@ -1,23 +1,58 @@
 import Carousel from "@/app/components/carousel";
+import clsx from "clsx";
 
-export default function HorizontalCard() {
+interface HorizontalCardProps {
+  order?: "normal" | "flip";
+}
+
+export default function HorizontalCard({
+  order = "normal",
+}: HorizontalCardProps) {
   return (
-    <div className="flex flex-col md:flex-row w-full max-w-4xl border border-palette-2 rounded-md overflow-hidden">
-      <section className="w-full md:w-1/2 p-4 border-r border-r-palette-2 bg-palette-2 bg-opacity-10">
-        <h2 className="text-xl text-center mb-2">College</h2>
-        <p className="p-2 text-sm">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-          odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-          quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
-          mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
-          Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
-          litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
-          sodales ligula in libero.{" "}
-        </p>
-      </section>
-      <section className="w-full md:w-1/2">
-        <Carousel />
-      </section>
+    <section className="flex flex-col md:flex-row w-full max-w-4xl border border-palette-2 rounded-md overflow-hidden mb-5">
+      {order === "flip" ? (
+        <>
+          <CarouselContainer />
+          <ContentContainer isFlipped={true} />
+        </>
+      ) : (
+        <>
+          <ContentContainer isFlipped={false} />
+          <CarouselContainer />
+        </>
+      )}
+    </section>
+  );
+}
+
+function CarouselContainer() {
+  return (
+    <div className="w-full md:w-1/2">
+      <Carousel />
+    </div>
+  );
+}
+
+function ContentContainer({ isFlipped }: { isFlipped: boolean }) {
+  return (
+    <div
+      className={clsx(
+        "w-full md:w-1/2 p-4  bg-palette-2 bg-opacity-10",
+        !isFlipped
+          ? "border-r border-r-palette-2"
+          : "border-l border-l-palette-2",
+      )}
+    >
+      <h2 className="text-xl text-center mb-2">College</h2>
+      <p className="p-2 text-sm">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
+        odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
+        quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
+        mauris. Fusce nec tellus sed augue semper porta. Mauris massa.
+        Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Curabitur
+        sodales ligula in libero.{" "}
+      </p>
     </div>
   );
 }
