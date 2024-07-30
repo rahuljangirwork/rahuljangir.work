6809434ee8b7bf832b39cb981f52d57dbeb87b77
isai7710 <isai10sanchez@gmail.com>
2024-07-30 14:27:35 -0600

    modified carousel to accept single src for image, will change to array and implement carousel behavior soon

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -2,19 +2,23 @@ import Image from "next/image";
 import clsx from "clsx";
 
 export default function Carousel({
-  isFlipped = false,
+  src,
+  side = "right",
 }: {
-  isFlipped?: boolean;
+  src: string;
+  side?: string;
 }) {
   return (
     <>
       <div className="relative w-full h-full">
         <Image
-          src={"/shepherd.jpg"}
+          src={src}
           layout="fill"
           objectFit="cover"
           alt="NEEDS GOOD ALT"
-          className={clsx(!isFlipped ? "rounded-r-md" : "rounded-l-md")}
+          className={clsx(
+            !(side === "right") ? "rounded-l-md" : "rounded-r-md",
+          )}
         />
       </div>
     </>
