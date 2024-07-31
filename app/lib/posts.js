329838cd9f7c46f3bd0f77a2c820c048817a0cff
isai7710 <isai10sanchez@gmail.com>
2024-07-30 19:33:15 -0600

    new function uses fs package along with gray matter to read the contents of a specific file in the posts directory given the slug (title) of the file

--- app/lib/posts.js
+++ app/lib/posts.js
@@ -37,3 +37,20 @@ export async function getSortedPostsData() {
     }
   });
 }
+
+export async function getPostData(slug) {
+  const filePath = path.join(postsDirectory, `${slug}.md`);
+  const fileContents = await fs.promises.readFile(filePath, "utf8");
+
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the id
+  return {
+    slug,
+    content: matterResult.content,
+    title: matterResult.data.title,
+    date: matterResult.data.date,
+    src: matterResult.data.coverImage,
+    ...matterResult.data,
+  };
+}
