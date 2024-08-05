8c04d74ae35ae926d60c3eb48b8fe3cba217c866
isai7710 <isai10sanchez@gmail.com>
2024-08-05 14:03:34 -0600

    lots of restructuring and rephrasing

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import CallToAction from "@/app/components/call-to-action";
 import { Separator } from "@/app/components/ui/separator";
 import {
   SplitCard,
@@ -13,80 +14,62 @@ import { cn } from "@/app/lib/utils";
 export default function Home() {
   return (
     <>
-      <section className="w-full px-2 md:max-w-4xl md:px-0 mx-auto text-palette-2">
-        <div className="flex flex-col md:flex-row justify-between items-center mt-7 mb-4 gap-4">
-          <Image
-            src="/mebw.JPG"
-            width={360}
-            height={240}
-            alt="photo of me"
-            className="hidden lg:block border-2 border-palette-1 rounded-xl"
-          />
-          <Image
-            src="/mebw.JPG"
-            width={300}
-            height={200}
-            alt="photo of me"
-            className="hidden md:block lg:hidden border-2 border-palette-1 rounded-xl"
-          />
-          <div className="mx-2 flex flex-col items-center">
-            <Image
-              src="/mebw.JPG"
-              width={240}
-              height={160}
-              alt="photo of me"
-              className="block md:hidden border-2 border-palette-1 rounded-xl"
-            />
-            <div className="py-2 max-w-lg md:w-full">
-              <div className="flex items-center space-x-4 pb-2">
-                <h1 className="text-center font-bold text-4xl inline-block">
-                  {`Hi, I'm Isai`}
-                </h1>
-                <Separator
-                  orientation="vertical"
-                  className="h-7 bg-palette-1"
-                />
-                <p
-                  className={cn(
-                    "italic text-md pr-1",
-                    "bg-gradient-to-r from-palette-2 to-palette-1",
-                    "text-transparent bg-clip-text",
-                  )}
-                >
-                  Welcome to my website!
-                </p>
-              </div>
-              <p className="text-md text-justify">
-                {`I've always wanted an online space to
-                share my notes and thoughts on a variety of topics that I've
-                been fortunate to experience during my schooling and in life.`}
-              </p>
-              <p className="text-md text-justify">
-                {`Here, you'll find some of my favorite projects and blog posts, covering
+      <section
+        className={cn(
+          "text-palette-2 w-full mx-auto mt-7 gap-4 px-2",
+          "lg:max-w-4xl lg:px-0",
+          "flex flex-col md:flex-row justify-between items-start",
+        )}
+      >
+        <Image
+          src="/mebw.JPG"
+          width={360}
+          height={240}
+          alt="photo of me"
+          className="border-2 border-palette-1 rounded-xl"
+        />
+        <div className="flex flex-col max-w-lg md:w-full">
+          <div className="flex items-center space-x-4 pb-2">
+            <h1 className="text-center font-bold text-6xl inline-block">
+              {`Hi, I'm Isai`}
+            </h1>
+            <Separator orientation="vertical" className="h-7 bg-palette-1" />
+            <p
+              className={cn(
+                "italic text-md pr-1 whitespace-nowrap",
+                "bg-gradient-to-r from-palette-2 to-palette-1",
+                "text-transparent bg-clip-text",
+              )}
+            >
+              Welcome to my website!
+            </p>
+          </div>
+          <p className="text-md text-justify">
+            {`Thanks for stopping by. Here, you'll find some of my projects, notes, and blog posts, covering
                 everything from how I set up my Minolta film camera to the
                 intricacies of the 2nd Law of Thermodynamics and everything in
                 between. I'd love to hear your feedback or chat about any of
                 these topics, so feel free to reach out! `}
-              </p>
-            </div>
-          </div>
+          </p>
+          <CallToAction />
         </div>
       </section>
-      <Separator className="w-[97%] md:w-full md:max-w-4xl mx-auto bg-palette-1" />
-      <section className="w-full px-2 md:max-w-4xl md:px-0 mx-auto text-palette-2 mb-3">
-        <h1 className="font-bold text-3xl py-3">About Me</h1>
+      <Separator className="w-[97%] md:w-full md:max-w-4xl mx-auto bg-palette-1 my-4" />
+      <section className="w-full px-2 md:max-w-4xl md:px-0 mx-auto text-palette-2 my-3">
+        <h1 className="font-bold text-3xl pb-3">About</h1>
         <section>
           <SplitCard>
             <SplitCardContent>
-              <SplitCardHeader>College</SplitCardHeader>
+              <SplitCardHeader>Education</SplitCardHeader>
               <SplitCardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-                nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed
-                nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis
-                ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.
-                Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis
-                sagittis ipsum. Praesent mauris. Praesent mauris.
+                <p className="text-md text-justify">
+                  I graduated with a Mechanical Engineering degree from Brigham
+                  Young University. There, I fostered a mindset of continuous
+                  learning and curiosity which I use to this day. My technical
+                  proficiencies span advanced CAD software, robotics systems,
+                  high-level math & physics, computer programming, and hardware
+                  integration.
+                </p>
               </SplitCardText>
             </SplitCardContent>
             <SplitCardThumbnail>
@@ -113,6 +96,9 @@ export default function Home() {
             </SplitCardContent>
           </SplitCard>
         </section>
+        <p className="text-md text-justify">
+          {`I've always wanted an online space to share my notes and thoughts on everything that I've been fortunate enough to experience throughout my schooling and life. All code is found on Github`}
+        </p>
       </section>
       <div className="w-full md:max-w-4xl mx-auto flex items-center text-palette-2 gap-2 mb-2">
         <Separator className="w-[80%] bg-palette-2" />
