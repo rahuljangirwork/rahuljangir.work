3d9b0847019c17dff4d740bf371942c2cd95c4b3
isai7710 <isai10sanchez@gmail.com>
2024-08-06 15:49:49 -0600

    added new icons and worked on their layout, still needs work

--- app/components/toolkit.tsx
+++ app/components/toolkit.tsx
@@ -1,23 +1,28 @@
-import { CppIcon, CIcon, PythonIcon, MatlabIcon } from "@/public/icons";
+import {
+  CppIcon,
+  PythonIcon,
+  MatlabIcon,
+  ArduinoIcon,
+  RaspberryPiIcon,
+} from "@/public/icons";
 import {
   ReactIcon,
   NextJsIcon,
-  JavascriptIcon,
   TypescriptIcon,
   TailwindIcon,
 } from "@/public/icons";
+import { SolidworksIcon } from "@/public/icons";
 
 export default function Toolkit() {
   return (
     <div className="w-3/4 mx-auto flex gap-4">
       <div className="w-1/3 flex-col items-center">
         <div className="w-1/2 mx-auto grid grid-cols-2 gap-1 content-center">
-          <CppIcon className="w-10 h-10 place-self-center" />
-          <CIcon className="w-10 h-10 place-self-center" />
+          <CppIcon className="col-span-2 w-10 h-10 place-self-center" />
           <PythonIcon className="w-10 h-10 place-self-center" />
           <MatlabIcon className="w-10 h-10 place-self-center" />
         </div>
-        <p className="text-center text-sm mt-2">Programming Languages</p>
+        <p className="text-center text-sm mt-2">Languages</p>
       </div>
       <div className="w-1/3 flex-col items-center">
         <div className="w-1/2 mx-auto grid grid-cols-2 gap-1 content-center">
@@ -26,16 +31,18 @@ export default function Toolkit() {
           <TailwindIcon className="w-10 h-10 place-self-center" />
           <TypescriptIcon className="w-10 h-10 place-self-center" />
         </div>
-        <p className="text-center text-sm mt-2">Website Development</p>
+        <p className="text-center text-sm mt-2">Frontend</p>
       </div>
-      <div className="w-1/3 flex-col items-center">
-        <div className="w-1/2 mx-auto grid grid-cols-2 gap-1 content-center">
-          <ReactIcon className="w-10 h-10 place-self-center" />
-          <NextJsIcon className="w-10 h-10 place-self-center" />
-          <TailwindIcon className="w-10 h-10 place-self-center" />
-          <TypescriptIcon className="w-10 h-10 place-self-center" />
+      <div className="w-1/3 flex flex-col items-center gap-4">
+        <div className="w-3/4 mx-auto flex gap-2 items-center">
+          <p className="text-center text-sm place-self-center">CAD</p>
+          <SolidworksIcon className="w-24 h-auto" />
         </div>
-        <p className="text-center text-sm mt-2">Website Development</p>
+        <div className="w-3/4 mx-auto flex gap-2 items-center">
+          <p className="text-center text-sm">Hardware</p>
+          <ArduinoIcon className="w-24 h-auto" />
+          <RaspberryPiIcon className="w-24 h-auto" />
+        </div>
       </div>
     </div>
   );
