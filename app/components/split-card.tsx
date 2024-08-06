d61b3d604e3a7b6786d5053da765cb09f051ec65
isai7710 <isai10sanchez@gmail.com>
2024-08-05 20:28:04 -0600

    split card text is now split card body since it won't only contain text as is shown in the second card in the home page, other fixes with parent components as well

--- app/components/split-card.tsx
+++ app/components/split-card.tsx
@@ -3,7 +3,13 @@ import { ReactNode } from "react";
 
 export function SplitCard({ children }: { children: ReactNode }) {
   return (
-    <section className="flex flex-col md:flex-row w-full max-w-4xl border-2 border-palette-1 shadow-xl rounded-md overflow-hidden mb-5">
+    <section
+      className={clsx(
+        "flex flex-col md:flex-row w-full max-w-4xl border-2 border-palette-1 shadow-xl rounded-md overflow-hidden mb-5",
+        "transition-all duration-200 ease-in-out",
+        "hover:scale-[1.01] hover:shadow-[0_10px_60px_25px_rgba(79,111,82,0.3)]",
+      )}
+    >
       {children}
     </section>
   );
@@ -19,7 +25,7 @@ export function SplitCardContent({
   return (
     <div
       className={clsx(
-        "p-4  bg-palette-2 bg-opacity-10",
+        "p-4 bg-palette-2 bg-opacity-10",
         side === "all" ? "w-full" : "w-1/2",
         side === "left" && "border-r border-r-palette-1",
         side === "right" && "border-l border-l-palette-1",
@@ -32,8 +38,8 @@ export function SplitCardContent({
 export function SplitCardHeader({ children }: { children: ReactNode }) {
   return <h2 className="text-xl text-center mb-2">{children}</h2>;
 }
-export function SplitCardText({ children }: { children: ReactNode }) {
-  return <h2 className="text-sm p-2">{children}</h2>;
+export function SplitCardBody({ children }: { children: ReactNode }) {
+  return <>{children}</>;
 }
 
 export function SplitCardThumbnail({ children }: { children: ReactNode }) {
