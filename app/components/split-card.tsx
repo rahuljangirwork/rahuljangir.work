1985a0f8bda8bea2bd5771b41293a9f19ff0c720
isai7710 <isai10sanchez@gmail.com>
2024-08-12 12:59:15 -0600

    added the cn utility function to this component to make it more modular, see home page.tsx for examples

--- app/components/split-card.tsx
+++ app/components/split-card.tsx
@@ -1,14 +1,19 @@
-import clsx from "clsx";
+import { cn } from "@/app/lib/utils";
 import { ReactNode } from "react";
 import { Separator } from "@/app/components/ui/separator";
 
-export function SplitCard({ children }: { children: ReactNode }) {
+export function SplitCard({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
   return (
     <section
-      className={clsx(
-        "flex flex-col md:flex-row w-full max-w-4xl border-2 border-palette-1 shadow-xl rounded-md overflow-hidden mb-5",
-        "transition-all duration-200 ease-in-out",
-        "hover:scale-[1.01] hover:shadow-[0_10px_60px_25px_rgba(79,111,82,0.3)]",
+      className={cn(
+        className,
+        "flex flex-col md:flex-row w-full max-w-4xl rounded-md overflow-hidden",
       )}
     >
       {children}
@@ -18,15 +23,18 @@ export function SplitCard({ children }: { children: ReactNode }) {
 
 export function SplitCardContent({
   children,
+  className,
   side = "left",
 }: {
   side?: "left" | "right" | "all";
+  className?: string;
   children: ReactNode;
 }) {
   return (
     <div
-      className={clsx(
-        "p-4 bg-palette-2 bg-opacity-10",
+      className={cn(
+        className,
+        "p-4",
         side === "all" ? "w-full" : "w-1/2",
         side === "left" && "border-r border-r-palette-1",
         side === "right" && "border-l border-l-palette-1",
@@ -36,14 +44,16 @@ export function SplitCardContent({
     </div>
   );
 }
+
 export function SplitCardHeader({ children }: { children: ReactNode }) {
   return (
-    <h2 className="text-xl text-center">
+    <>
       {children}
       <Separator className="w-[40px] mx-auto mt-1 mb-2 bg-palette-4" />
-    </h2>
+    </>
   );
 }
+
 export function SplitCardBody({ children }: { children: ReactNode }) {
   return <>{children}</>;
 }
