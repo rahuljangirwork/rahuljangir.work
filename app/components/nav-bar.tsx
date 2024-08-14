d3111883127d17f8978d4eac9c382e8ec9ffaf7d
isai7710 <isai10sanchez@gmail.com>
2024-08-14 15:41:29 -0600

    small changes to nav bar, takes up less space in the header now

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -8,6 +8,7 @@ import { Menu, X } from "react-feather";
 import { TentTreeIcon } from "lucide-react";
 import { useState } from "react";
 import DropdownMenu from "@/app/components/dropdown-menu";
+import { Separator } from "@/app/components/ui/separator";
 
 const links = [
   {
@@ -29,14 +30,14 @@ export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 z-50 mb-2 bg-primary bg-opacity-25 border-palette-1 border-b shadow-xl backdrop-blur-sm transition-all duration-200">
+    <header className="w-full lg:max-w-5xl mx-auto lg:rounded-md sticky top-2 z-50 mb-2 bg-primary bg-opacity-25 border-palette-1 border-b lg:border shadow-xl backdrop-blur-sm transition-all duration-200">
       <nav className="flex items-center h-16 px-4 text-palette-3 max-w-7xl mx-auto">
         <div className="flex-1 flex justify-start">
           <Link
             href="/"
             className={clsx(
               "flex items-center gap-1 text-center font-semibold text-md bg-transparent md:text-md lg:text-xl",
-              "transition-all duration-300 ease-in-out hover:scale-[1.08] hover:rotate-12",
+              "transition-all duration-300 ease-in-out hover:scale-[1.08]",
               pathname === "/" && "text-palette-4",
             )}
           >
@@ -44,13 +45,13 @@ export default function NavBar() {
             Home
           </Link>
         </div>
-        <ul className="flex-1 justify-center space-x-5 hidden md:flex">
+        <ul className="flex-1 hidden justify-center items-center space-x-2 md:flex">
           {links.map((link) => (
             <li key={link.name}>
               <Link
                 href={link.href}
                 className={clsx(
-                  "font-medium px-3 py-2 rounded-md transition-all duration-200",
+                  "text-md px-3 py-2 rounded-md transition-all duration-200",
                   pathname === link.href
                     ? "text-palette-4 bg-palette-2 bg-opacity-10 underline underline-offset-2"
                     : "text-palette-3 hover:underline hover:bg-palette-2 hover:bg-opacity-10 hover:underline-offset-2 hover:shadow-xl",
@@ -61,7 +62,7 @@ export default function NavBar() {
             </li>
           ))}
         </ul>
-        <div className="flex-1 justify-end hidden md:flex">
+        <div className="flex-1 justify-end hidden md:flex gap-1">
           <Socials />
         </div>
 
