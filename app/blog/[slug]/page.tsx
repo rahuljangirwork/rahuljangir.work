9a1a641c99f514fdd82aed7b5ea94dae5819ad06
isai7710 <isai10sanchez@gmail.com>
2024-08-14 15:39:24 -0600

    reflects name change of file that fetches post and post data

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,7 +2,7 @@ import Markdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
-import { getPostData } from "@/app/lib/posts";
+import { getPostData } from "@/app/lib/blogposts";
 
 export default async function Post({ params }: { params: { slug: string } }) {
   const postData = await getPostData(params.slug);
