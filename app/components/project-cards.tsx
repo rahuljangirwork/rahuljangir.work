1dcc648c59c6a0f3963c8f0c9cf0b6ab16361981
isai7710 <isai10sanchez@gmail.com>
2024-08-16 12:32:08 -0600

    created new project cards component, working on its layout and style now

---
+++ app/components/project-cards.tsx
@@ -0,0 +1,37 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
+import { getSortedPostsData } from "@/app/lib/blogposts";
+import Link from "next/link";
+import { cn } from "@/app/lib/utils";
+import Markdown from "react-markdown";
+
+export default async function ProjectCards() {
+  const allPostsData = await getSortedPostsData();
+  return (
+    <div className="w-full max-w-4xl mx-auto px-4 text-palette-3">
+      <ul className="grid grid-cols-2 md:grid-cols-3 gap-3">
+        {allPostsData.map(({ id, date, description, title }) => (
+          <li key={id}>
+            <Card className="flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full">
+              <Link href={`/blog/${id}`} className={cn("block cursor-pointer")}>
+                <CardHeader className="px-2">
+                  <div className="space-y-1">
+                    <CardTitle className="mt-1 text-base">{title}</CardTitle>
+                    <time className="font-sans text-xs">{date}</time>
+                    <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
+                      {description}
+                    </Markdown>
+                  </div>
+                </CardHeader>
+              </Link>
+            </Card>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
