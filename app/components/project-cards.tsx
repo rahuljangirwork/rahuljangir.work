cf2b9d462a8bbf8795a7933815ab7eb694718c84
isai7710 <isai10sanchez@gmail.com>
2024-08-19 10:52:48 -0600

    simplified by removing imports that aren't needed as of yet

--- app/components/project-cards.tsx
+++ app/components/project-cards.tsx
@@ -1,37 +1,33 @@
 import {
   Card,
-  CardDescription,
+  CardContent,
+  CardFooter,
   CardHeader,
   CardTitle,
+  CardDescription,
 } from "@/app/components/ui/card";
 import { getSortedPostsData } from "@/app/lib/projectposts";
 import Link from "next/link";
-import { cn } from "@/app/lib/utils";
-import Markdown from "react-markdown";
 
-export default async function ProjectCards() {
+export default async function ProjectCards({
+  className,
+}: {
+  className?: string;
+}) {
   const allPostsData = await getSortedPostsData();
   return (
-    <div className="w-full max-w-4xl mx-auto px-4 mb-8">
-      <ul className="flex justify-between gap-3">
-        {allPostsData.map(({ id, date, description, title }) => (
+    <div className="w-full max-w-4xl mx-auto px-4 text-palette-3 mb-12">
+      <ul className="flex gap-3">
+        {allPostsData.map(({ id, date, title }) => (
           <li key={id}>
-            <Card className="flex flex-col overflow-hidden border hover:shadow-lg transition-all duration-300 ease-out h-full">
-              <Link
-                href={`/projects/${id}`}
-                className={cn("block cursor-pointer")}
-              >
-                <CardHeader className="px-2">
-                  <div className="space-y-1">
-                    <CardTitle className="mt-1 text-base">{title}</CardTitle>
-                    <time className="font-sans text-xs">{date}</time>
-                    <Markdown className="prose max-w-full text-pretty font-sans text-xs text-muted-foreground dark:prose-invert">
-                      {description}
-                    </Markdown>
-                  </div>
+            <Link href={`/projects/${id}`}>
+              <Card className="flex flex-col h-full">
+                <CardHeader className="flex-grow">
+                  <CardTitle>{title}</CardTitle>
+                  <CardDescription>{date}</CardDescription>
                 </CardHeader>
-              </Link>
-            </Card>
+              </Card>
+            </Link>
           </li>
         ))}
       </ul>
