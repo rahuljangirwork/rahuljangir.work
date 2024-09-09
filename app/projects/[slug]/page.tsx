1b0c70a9ebbdae022724690453a6763b54cec126
isai7710 <isai10sanchez@gmail.com>
2024-09-09 17:24:17 -0600

    trying to add custom components for react markdown to no avail

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -2,7 +2,17 @@ import Markdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
+import Collapsible from "@/app/components/collapsible";
 import { getPostData } from "@/app/lib/projectposts";
+import { ReactNode } from "react";
+
+const renderers = {
+  code: ({ language, ...props }: { language: string }) => {
+    if (language === "react") {
+      return <Collapsible title={title}>{children}</Collapsible>;
+    }
+  },
+};
 
 export default async function Post({ params }: { params: { slug: string } }) {
   const postData = await getPostData(params.slug);
@@ -10,7 +20,11 @@ export default async function Post({ params }: { params: { slug: string } }) {
   return (
     <div className="flex flex-col justify-center items-center">
       <div key={postData.title} className="prose prose-offwhite w-full m-1">
-        <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+        <Markdown
+          remarkPlugins={[remarkMath]}
+          rehypePlugins={[rehypeKatex]}
+          components={components}
+        >
           {postData.content}
         </Markdown>
       </div>
