b3cb84544670e3fb561d9476dc95df73d9de8fad
isai7710 <isai10sanchez@gmail.com>
2024-09-09 15:19:57 -0600

    react markdown wrapped in tracing beam component

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,20 +1,27 @@
 import Markdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
+import remarkGfm from "remark-gfm";
 import "katex/dist/katex.min.css";
 import { getPostData } from "@/app/lib/blogposts";
+import { TracingBeam } from "@/app/components/ui/tracing-beam";
 
 export default async function Post({ params }: { params: { slug: string } }) {
   const postData = await getPostData(params.slug);
 
   return (
-    <div
-      key={postData.title}
-      className="mx-auto prose prose-offwhite w-full mt-4 px-6"
-    >
-      <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
-        {postData.content}
-      </Markdown>
-    </div>
+    <TracingBeam className="px-6">
+      <div
+        key={postData.title}
+        className="mx-auto prose prose-offwhite w-full mt-8 px-6"
+      >
+        <Markdown
+          remarkPlugins={[remarkMath, remarkGfm]}
+          rehypePlugins={[rehypeKatex]}
+        >
+          {postData.content}
+        </Markdown>
+      </div>
+    </TracingBeam>
   );
 }
