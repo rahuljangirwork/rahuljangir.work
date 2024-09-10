3c3119e2e82886dd290f4829d89e6f58b034c0ab
isai7710 <isai10sanchez@gmail.com>
2024-09-10 16:12:24 -0600

    custom mdx component file to overwrite certain jsx generated by mdx and add my own react components if necessary too

---
+++ app/components/mdx.tsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import Image, { ImageProps } from "next/image";
+import React from "react";
+import Collapsible from "./collapsible";
+
+type TableProps = {
+  data: {
+    headers: string[];
+    rows: string[][];
+  };
+};
+
+const Table: React.FC<TableProps> = ({ data }) => {
+  const headers = data.headers.map((header, index) => (
+    <th key={index}>{header}</th>
+  ));
+  const rows = data.rows.map((row, index) => (
+    <tr key={index}>
+      {row.map((cell, cellIndex) => (
+        <td key={cellIndex}>{cell}</td>
+      ))}
+    </tr>
+  ));
+
+  return (
+    <table>
+      <thead>
+        <tr>{headers}</tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+};
+
+type CustomLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+const CustomLink: React.FC<CustomLinkProps> = (props) => {
+  const { href } = props;
+
+  if (href && href.startsWith("/")) {
+    return (
+      <Link href={href} {...props}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  if (href && href.startsWith("#")) {
+    return <a {...props} />;
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+};
+
+const RoundedImage: React.FC<ImageProps> = (props) => {
+  return <Image alt={props.alt} className="rounded-lg" {...props} />;
+};
+
+export const CustomMDXComponents = {
+  Image: RoundedImage,
+  a: CustomLink,
+  Table,
+  Collapsible,
+};
