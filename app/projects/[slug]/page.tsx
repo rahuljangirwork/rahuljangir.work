2e30c5fabda20c693289bdd14dec7696a58b72cb
isai7710 <isai10sanchez@gmail.com>
2024-09-10 16:16:35 -0600

    some changes to functionality and testing

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,19 +1,23 @@
 import { getPostBySlug, getAllPostsSlug } from "@/app/lib/projectposts";
 
 export async function generateStaticParams() {
-  return getAllPostsSlug();
+  const slugs = getAllPostsSlug();
+
+  return slugs.map(({ slug }) => ({
+    slug,
+  }));
 }
 
 export default async function Post({ params }: { params: { slug: string } }) {
   const post = await getPostBySlug(params.slug);
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <h1>{post.frontmatter.title}</h1>
+    <div className="flex flex-col justify-center items-center text-palette-2">
+      <h1 className="mt-2">{post.frontmatter.title}</h1>
       <p>Description of project: {post.frontmatter.description}</p>
       <article
         key={post.frontmatter.title}
-        className="prose prose-offwhite w-full m-1"
+        className="prose prose-offwhite w-full m-2"
       >
         {post.content}
       </article>
