1f6b2215deb3c42d8161565e7cc092d40b3a431a
isai7710 <isai10sanchez@gmail.com>
2024-09-10 00:34:20 -0600

    rewrote to better visualize importing react components in mdx

--- app/components/collapsible.tsx
+++ app/components/collapsible.tsx
@@ -2,25 +2,23 @@
 import { useState } from "react";
 
 type CollapsibleProps = {
-  title: string;
   children: React.ReactNode;
 };
 
-const Collapsible: React.FC<CollapsibleProps> = ({ title, children }) => {
+const Collapsible: React.FC<CollapsibleProps> = ({ children }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => setIsOpen(!isOpen);
 
   return (
-    <div className="border-b border-gray-300 mb-4">
+    <div className="border border-gray-300 mb-4">
       <button
         onClick={toggleOpen}
         className="w-full text-left font-semibold text-lg focus:outline-none p-2"
       >
-        {title}
         <span className="ml-2">{isOpen ? "▲" : "▼"}</span>
       </button>
-      {isOpen && <div className="p-4 bg-gray-100">{children}</div>}
+      {isOpen && <div className="p-4 bg-transparent">{children}</div>}
     </div>
   );
 };
