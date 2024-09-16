b99e37b19c6936e172f96902e62cc20a5649661f
isai7710 <isai10sanchez@gmail.com>
2024-09-16 17:09:37 -0600

    lots more responsiveness all over the place, also intro image is circular now and responsive

--- app/page.tsx
+++ app/page.tsx
@@ -21,15 +21,14 @@ export default function Home() {
     <>
       <section
         className={cn(
-          "relative text-palette-2 max-w-2xl mx-auto gap-2 px-2 my-20",
-          "md:max-w-3xl",
-          "lg:max-w-4xl lg:px-0 lg:gap-4",
-          "flex flex-row items-center",
+          "relative text-palette-2 w-full max-w-2xl mx-auto gap-2 px-4 md:px-0 my-20",
+          "lg:max-w-3xl lg:px-0 lg:gap-4",
+          "flex flex-row justify-between items-center",
         )}
       >
         <div className="flex flex-col max-w-lg md:w-full h-full">
           <motion.h1
-            className="relative font-bold text-4xl sm:text-6xl text-left pb-1"
+            className="relative font-bold text-4xl sm:text-5xl md:text-6xl text-left pb-1"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
@@ -37,17 +36,20 @@ export default function Home() {
             Hi<span className="text-palette-4">,</span> I&apos;m Isai
           </motion.h1>
           <motion.p
-            className="w-full text-lg lg:text-md"
+            className="w-full text-md lg:text-lg"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.2, duration: 0.5 }}
           >
             Mechanical Engineer and freelance Software Developer trained in CAD
-            modeling, robotics, and web development. I love transforming
-            innovative designs to life.
+            modeling, robotics, and web development.
+            <span className="hidden sm:inline">
+              {" "}
+              I love transforming innovative designs to life.
+            </span>
           </motion.p>
           <motion.div
-            className="hidden lg:flex mt-2"
+            className="hidden sm:flex mt-2"
             initial={{ opacity: 0, scale: 1 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ delay: 0.6, duration: 0.5 }}
@@ -55,17 +57,17 @@ export default function Home() {
             <CallToAction className="py-2" />
           </motion.div>
         </div>
-        <Image
-          src="/mebw.JPG"
-          width={360}
-          height={240}
-          alt="photo of me"
-          className={cn(
-            "border-2 border-palette-1",
-            "w-16 h-full rounded-full", // Mobile styles
-            "md:w-auto md:h-auto md:rounded-xl", // Desktop styles
-          )}
-        />
+        <div className="relative w-full min-w-[120px] max-w-[200px] aspect-square">
+          <Image
+            src="/mebw.JPG"
+            alt="Mirror film selfie"
+            fill
+            className={cn(
+              "border-4 border-palette-1 rounded-full object-cover",
+              "transition-transform duration-300 ease-in-out hover:scale-105",
+            )}
+          />
+        </div>
       </section>
       <section className="w-full md:max-w-4xl md:px-0 mx-auto text-palette-2">
         <SplitCard className="mb-20">
