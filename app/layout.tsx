0b9f557293809c8648053de556ce499e687533f8
isai7710 <isai10sanchez@gmail.com>
2024-10-09 13:45:15 -0600

    moved footer to its own component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import { space_grotesk } from "@/app/lib/fonts";
 import "@/app/globals.css";
 import NavBar from "@/app/components/navigation/nav-bar";
+import Footer from "./components/footer";
 import { Toaster } from "@/app/components/ui/toaster";
-import { Mail } from "react-feather";
 
 export const metadata: Metadata = {
   title: "Isai Portfolio",
@@ -23,22 +23,7 @@ export default function RootLayout({
         <NavBar />
         <main>{children}</main>
         <Toaster />
-        <footer className="bg-palette-2 bg-opacity-10 text-palette-2 text-opacity-50 mt-4 p-2 z-20 relative">
-          <div className="flex justify-between items-center">
-            <div className="w-full md:w-1/3 mb-4 md:mb-0">
-              <p className="text-sm">
-                © 2024 Isai Sanchez. All rights reserved.
-              </p>
-            </div>
-            <div className="flex justify-between items-center space-x-2">
-              <h4 className="font-bold pr-2">
-                <a href="mailto:isaisanchezcc@gmail.com">
-                  <Mail className="w-5" />
-                </a>
-              </h4>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </body>
     </html>
   );
