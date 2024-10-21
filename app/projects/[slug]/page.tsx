224d51777e2b6353234eab2063780877cc047bf7
isai7710 <isai10sanchez@gmail.com>
2024-10-21 11:45:03 -0600

    cleaned up project pages

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -12,9 +12,7 @@ export default async function Post({ params }: { params: { slug: string } }) {
   const post = await getPostBySlug(params.slug);
 
   return (
-    <div className="flex flex-col justify-center items-center text-palette-2">
-      <h1 className="mt-2">{post.frontmatter.title}</h1>
-      <p>Description of project: {post.frontmatter.description}</p>
+    <div className="flex flex-col justify-center items-center text-palette-2 mt-12">
       <article
         key={post.frontmatter.title}
         className="prose prose-offwhite w-full m-2"
