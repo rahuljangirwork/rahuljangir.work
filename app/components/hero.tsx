b041b9b04db3964d9ccf33222d9e585d4bbcd212
isai7710 <isai10sanchez@gmail.com>
2024-10-21 17:08:45 -0600

    happier with responsiveness

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -6,30 +6,20 @@ import CallToAction from "@/app/components/navigation/call-to-action";
 
 export default function Hero() {
   return (
-    <section className="flex flex-col items-center my-10 md:my-20">
-      <div className="text-palette-2 flex justify-between items-start md:items-center gap-2">
-        <div className="flex flex-col sm:gap-2 text-left">
-          <motion.div
-            className="flex gap-2 items-center justify-between"
+    <section className="my-10 md:my-20">
+      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-8 md:gap-0 text-palette-2">
+        <div className="flex flex-col gap-2 text-center md:text-left">
+          <motion.h1
+            className="font-bold text-5xl md:text-6xl"
             initial={{ opacity: 0, x: -20 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
           >
-            <h1 className="font-bold text-5xl md:text-6xl">
-              Hi<span className="text-palette-4">,</span> I&apos;m Isai
-              <span className="text-palette-4">.</span>
-            </h1>
-            <div className="relative sm:hidden aspect-square w-16 h-fit">
-              <Image
-                src="/assets/mebw.JPG"
-                alt="Mirror film selfie"
-                fill
-                className="border-4 border-palette-1 rounded-full object-cover"
-              />
-            </div>
-          </motion.div>
+            Hi<span className="text-palette-4">,</span> I&apos;m Isai
+            <span className="text-palette-4">.</span>
+          </motion.h1>
           <motion.p
-            className="text-md md:text-lg max-w-2xl"
+            className="text-md md:text-lg text-balance"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.2, duration: 0.5 }}
@@ -39,9 +29,10 @@ export default function Hero() {
             trained in CAD, robotics, and simple web development. I love
             transforming innovative designs to life.
           </motion.p>
+          <CallToAction className="mt-4 w-full max-w-xs sm:max-w-lg md:max-w-xl mx-auto" />
         </div>
         <motion.div
-          className="hidden sm:block relative aspect-square w-48 md:w-56 h-fit"
+          className="relative aspect-square w-60 sm:w-72 md:w-96 h-fit"
           initial={{ opacity: 0, x: 20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.5 }}
@@ -50,11 +41,10 @@ export default function Hero() {
             src="/assets/mebw.JPG"
             alt="Mirror film selfie"
             fill
-            className="border-4 border-palette-1 rounded-full object-cover"
+            className="border-4 border-palette-1 rounded-full object-cover shadow-2xl"
           />
         </motion.div>
       </div>
-      <CallToAction className="mt-4 sm:mt-9" />
     </section>
   );
 }
