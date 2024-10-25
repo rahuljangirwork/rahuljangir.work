19d4ae8baefc6d0768e9b24b49317b5d0bb3f310
Isai Sanzy <isai10sanchez@gmail.com>
2024-10-25 14:02:58 -0600

    reverted back to shadcn ui card component for project cards, added logic to display project thumbnail based on whether an image string, video string, or robot scene boolean exist

--- app/components/cards/project-cards.tsx
+++ app/components/cards/project-cards.tsx
@@ -1,15 +1,17 @@
 import {
-  SplitCard,
-  SplitCardContent,
-  SplitCardHeader,
-  SplitCardBody,
-  SplitCardThumbnail,
-} from "@/app/components/cards/split-card";
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/app/components/ui/card";
 import { Badge } from "@/app/components/ui/badge";
 import { getSortedPosts } from "@/app/lib/projectposts";
 import Link from "next/link";
 import Image from "next/image";
 import { cn } from "@/app/lib/utils";
+import Scene from "../model/scene";
+import { Move } from "lucide-react";
 
 export default async function ProjectCards({
   className,
@@ -21,52 +23,69 @@ export default async function ProjectCards({
     <>
       {posts.map(({ slug, frontmatter }) => {
         return (
-          <SplitCard
+          <Card
             key={slug}
             className={cn(
               className,
-              "flex flex-row overflow-hidden border border-palette-1 shadow-xl",
+              "flex flex-col overflow-hidden border h-full border-palette-1",
             )}
           >
-            <SplitCardContent side="left" className="w-2/3">
-              <SplitCardHeader className="px-2">
-                <Link href={`/projects/${slug}`}>
-                  <div className="flex flex-col p-1">
-                    <h1 className="mt-1 text-lg">{frontmatter.title}</h1>
-                    <p className="font-sans text-xs text-pretty text-palette-2 text-opacity-50">
-                      {frontmatter.description}
-                    </p>
-                  </div>
-                </Link>
-              </SplitCardHeader>
-            </SplitCardContent>
-            <SplitCardThumbnail>
-              <Link href={`/projects/${slug}`}>
-                {frontmatter.image && (
-                  <div className="relative w-full aspect-[5/3] overflow-hidden">
-                    <Image
-                      src={frontmatter.image}
-                      alt={frontmatter.title}
-                      fill
-                      className="object-top"
-                    />
-                  </div>
-                )}
-                {frontmatter.video && (
-                  <div className="relative w-full aspect-[5/3] overflow-hidden">
-                    <video
-                      src={frontmatter.video}
-                      autoPlay
-                      loop
-                      muted
-                      playsInline
-                      className="object-top"
-                    />
-                  </div>
-                )}
-              </Link>
-            </SplitCardThumbnail>
-          </SplitCard>
+            {frontmatter.src && frontmatter.src.image && (
+              <div className="relative w-full aspect-[5/3] overflow-hidden">
+                <Image
+                  src={frontmatter.src.image}
+                  alt={frontmatter.title}
+                  fill
+                  className="object-top"
+                />
+              </div>
+            )}
+            {frontmatter.src && frontmatter.src.video && (
+              <div className="relative w-full aspect-[5/3] overflow-hidden">
+                <video
+                  src={frontmatter.src.video}
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                  className="object-top"
+                />
+              </div>
+            )}
+            {frontmatter.src && frontmatter.src.scene && (
+              <div className="relative w-full aspect-[5/3] overflow-hidden">
+                <Scene className="border-b border-b-palette-1 bg-white/5" />
+                <Move className="absolute right-1 bottom-1" />
+              </div>
+            )}
+            <Link href={`/projects/${slug}`}>
+              <CardHeader className="px-2">
+                <div className="flex flex-col p-1">
+                  <CardTitle className="mt-1 text-lg">
+                    {frontmatter.title}
+                  </CardTitle>
+                  <time className="font-sans text-xs text-palette-1">
+                    {frontmatter.publishDate}
+                  </time>
+                </div>
+              </CardHeader>
+              <CardContent className="mt-auto flex flex-row px-3 pb-3">
+                {frontmatter.technologies &&
+                  frontmatter.technologies.length > 0 && (
+                    <div className="mt-2 flex flex-wrap gap-1">
+                      {frontmatter.technologies?.map((tag) => (
+                        <Badge
+                          className="px-1 py-0 text-[10px] bg-white/10"
+                          key={tag.name}
+                        >
+                          {tag.name}
+                        </Badge>
+                      ))}
+                    </div>
+                  )}
+              </CardContent>
+            </Link>
+          </Card>
         );
       })}
     </>
