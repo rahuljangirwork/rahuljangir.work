9014de101109f806574136b630198386b4bd82c2
Isai Sanzy <isai10sanchez@gmail.com>
2024-10-25 12:36:42 -0600

    restructured to include new components and simplified, also max width expanded by one stop

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,35 @@
-import Link from "next/link";
 import {
   SplitCard,
   SplitCardContent,
   SplitCardHeader,
   SplitCardBody,
-  SplitCardThumbnail,
 } from "@/app/components/cards/split-card";
-import Carousel from "@/app/components/media/carousel";
-import Toolkit from "@/app/components/media/toolkit";
 import { cn } from "@/app/lib/utils";
-import EmailToast from "@/app/components/ui/email-toast";
 import ProjectCards from "@/app/components/cards/project-cards";
 import Hero from "./components/hero";
 import About from "./components/about";
+import BentoGrid from "./components/cards/bento-grid";
 
 export default function Home() {
   return (
-    <main className="max-w-2xl mx-auto px-4 md:px-0">
+    <main className="max-w-3xl mx-auto px-4 md:px-0">
       <Hero />
-      <About />
-      <section className="text-palette-2">
-        <div id="projects" className="scroll-mt-24">
-          <SplitCard>
-            <SplitCardContent side="all">
-              <SplitCardHeader>
-                <h1 className="text-left font-bold text-3xl">
-                  Projects<span className="text-palette-4">.</span>
-                </h1>
-              </SplitCardHeader>
-              <SplitCardBody>
-                <p className="text-md text-left">
-                  Here&apos;s a little bit of what I&apos;ve been working on.
-                </p>
-              </SplitCardBody>
-            </SplitCardContent>
-          </SplitCard>
-        </div>
+      <BentoGrid />
+      <section id="projects" className="scroll-mt-24 text-palette-2">
+        <SplitCard>
+          <SplitCardContent side="all">
+            <SplitCardHeader>
+              <h1 className="text-left font-bold text-3xl">
+                Projects<span className="text-palette-4">.</span>
+              </h1>
+            </SplitCardHeader>
+            <SplitCardBody>
+              <p className="text-md text-left">
+                Here&apos;s a little bit of what I&apos;ve been working on.
+              </p>
+            </SplitCardBody>
+          </SplitCardContent>
+        </SplitCard>
         <div className="grid grid-cols-1 gap-3 py-4 mb-20">
           <ProjectCards
             className={cn(
@@ -44,63 +38,8 @@ export default function Home() {
             )}
           />
         </div>
-        <SplitCard className="mb-5">
-          <SplitCardContent className="bg-palette-2 bg-opacity-10">
-            <SplitCardHeader>
-              <h2 className="text-xl text-center">Education</h2>
-            </SplitCardHeader>
-            <SplitCardBody>
-              <p className="text-sm px-2 text-justify">
-                I graduated with a Mechanical Engineering degree from Brigham
-                Young University. There, I fostered a mindset of continuous
-                learning and curiosity which I use to this day. My technical
-                proficiencies span advanced CAD software, robotics systems,
-                high-level math & physics, computer programming, and hardware
-                integration.
-              </p>
-            </SplitCardBody>
-          </SplitCardContent>
-          <SplitCardThumbnail>
-            <Carousel src="/assets/shepherd.jpg" />
-          </SplitCardThumbnail>
-        </SplitCard>
-        <SplitCard className="mb-5">
-          <SplitCardContent
-            side="all"
-            className="relative bg-palette-2 bg-opacity-10"
-          >
-            <SplitCardHeader>
-              <h2 className="text-xl text-center">My Toolkit</h2>
-            </SplitCardHeader>
-            <SplitCardBody>
-              <Toolkit />
-              <p className="absolute right-0 bottom-0 p-1 text-xs italic text-palette-2">
-                Check out my
-                <Link
-                  href="#projects"
-                  className="text-palette-4 hover:text-palette-1 transition-colors px-1"
-                >
-                  projects
-                </Link>
-                to see these in action
-              </p>
-            </SplitCardBody>
-          </SplitCardContent>
-        </SplitCard>
-        <SplitCard className="mb-5">
-          <SplitCardContent side="all" className="bg-palette-2 bg-opacity-10">
-            <SplitCardHeader>
-              <h2 className="text-xl text-center">Contact Me</h2>
-            </SplitCardHeader>
-            <SplitCardBody>
-              <p className="text-md px-2 text-center">
-                Feel free to shoot me an email at
-                <EmailToast className="inline-flex gap-1 text-md text-palette-4 hover:underline focus:outline-none" />
-              </p>
-            </SplitCardBody>
-          </SplitCardContent>
-        </SplitCard>
       </section>
+      <About />
     </main>
   );
 }
