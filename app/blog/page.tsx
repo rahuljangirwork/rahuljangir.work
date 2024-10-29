0bd2457c3e95523f5e045ace875705dcee162a35
Isai Sanzy <isai10sanchez@gmail.com>
2024-10-29 14:42:21 -0600

    made main page.tsx file a server side component that fetches all the blog post data and passes it down to children server components for better interactivity

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,36 +1,12 @@
-import BlogCards from "@/app/components/cards/blog-cards";
-import { Separator } from "@/app/components/ui/separator";
-import { PenTool } from "lucide-react";
+import { getSortedPostsMetaData } from "@/app/lib/blogposts";
+import BlogPage from "../components/blog-page";
 
 export default async function Blog() {
+  const allPostsData = await getSortedPostsMetaData();
+
   return (
-    <>
-      <div className="flex flex-col items-center w-full max-w-5xl mx-auto mt-6 text-palette-3">
-        <h2 className="flex items-center text-3xl py-2 gap-2">
-          <PenTool />
-          Blog Posts
-        </h2>
-        <Separator className="my-2 bg-palette-1" />
-        <div className="flex items-center space-x-8 py-3">
-          <ul>
-            <h3>Contents</h3>
-            <li>One</li>
-            <li>Two</li>
-            <li>Three</li>
-          </ul>
-          <Separator orientation="vertical" className="h-[80px] bg-palette-1" />
-          <p className="w-full text-palette-3 bold py-2">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec
-            odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla
-            quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
-            Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis
-            sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent
-            mauris. Praesent mauris.
-          </p>
-        </div>
-      </div>
-      <BlogCards />
-    </>
+    <main className="mt-8 flex flex-col md:flex-row md:items-start w-full max-w-4xl mx-auto text-palette-3 min-h-[70vh]">
+      <BlogPage initialPosts={allPostsData} />
+    </main>
   );
 }
