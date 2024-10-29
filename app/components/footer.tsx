59dda7504817b9be71855ceca5104f80a68e318e
Isai Sanzy <isai10sanchez@gmail.com>
2024-10-29 12:36:35 -0600

    removed bottom margin

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="bg-palette-2/10 backdrop-blur-md mt-4 p-2 z-20 relative">
       <div className="flex justify-between items-center text-palette-2/70">
-        <div className="w-full md:w-1/3 mb-4 md:mb-0">
+        <div className="w-full md:w-1/3">
           <p className="text-sm">© 2024 Isai Sanchez. All rights reserved.</p>
         </div>
         <div className="flex justify-between items-center space-x-2">
