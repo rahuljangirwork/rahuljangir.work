026366ac9fce3d446e09ea164a4b3ea0ae4aac78
Isai Sanzy <isai10sanchez@gmail.com>
2024-10-30 11:38:18 -0600

    fixed scene background and width

--- app/components/mdx/robot-scene.tsx
+++ app/components/mdx/robot-scene.tsx
@@ -8,11 +8,11 @@ const RobotScene = () => {
 
   return (
     <div
-      className="mb-12 relative w-full max-w-md mx-auto h-56 px-2"
+      className="mb-12 relative w-full max-w-lg mx-auto h-56 px-2"
       onMouseDown={() => setIsClicking(true)}
       onMouseUp={() => setIsClicking(false)}
     >
-      <Scene className="border border-palette-1 rounded-md" />
+      <Scene className="border border-palette-1 bg-primary rounded-md shadow-lg" />
       <span className="absolute bottom-0 right-2 text-palette-2 p-1">
         {isClicking ? <Grab /> : <Pointer />}
       </span>
