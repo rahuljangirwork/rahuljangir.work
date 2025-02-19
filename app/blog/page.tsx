afb78cd7c7e2cce27f93c52ee4af0c1901ead290
Isai Sanzy <isai10sanchez@gmail.com>
2025-02-19 15:05:57 -0700

    modificiations made to the blog post page and components for filtering, thumbnail addition, and more

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,37 @@
 import { getSortedPostsMetaData } from "@/app/lib/posts";
-import BlogPage from "@/app/components/blog-page";
+import BlogCards from "@/app/components/cards/blog-cards";
+import type { PostMetadata } from "@/app/lib/posts";
+import EmailToast from "@/app/components/ui/email-toast";
+import { MailPlus } from "lucide-react";
+import FilterDropdown from "@/app/components/navigation/filter-dropdown";
 
 export default async function Blog() {
-  const allPostsData = await getSortedPostsMetaData();
+  const allPostsData: PostMetadata[] = await getSortedPostsMetaData();
 
   return (
-    <main className="mt-8 flex flex-col md:flex-row md:items-start w-full max-w-4xl mx-auto text-palette-3 min-h-[70vh]">
-      <BlogPage posts={allPostsData} />
+    <main className="mt-8 flex flex-col md:flex-row md:items-start w-full max-w-4xl gap-2 px-4 lg:px-0 mx-auto text-palette-3 min-h-[70vh]">
+      <section className="pb-2 text-palette-2 w-full md:w-1/3 flex flex-col items-start">
+        <h1 className="text-4xl font-extrabold">
+          Blog Posts<span className="text-palette-4">.</span>
+        </h1>
+        <p className="text-lg mt-2">
+          Personal insights on small worlds within engineering and my life
+          <span className="text-palette-4">.</span>
+        </p>
+        {/*
+        <p className="text-palette-2 hover:text-palette-4 text-sm flex items-center mt-2 transition-all duration-300 ease-in-out hover:cursor-pointer">
+          Shoot me an email
+          <EmailToast iconSize={0}>
+            <MailPlus className="w-4 h-4" />
+          </EmailToast>
+        </p>
+        */}
+      </section>
+
+      <section className="w-full mx-auto md:w-2/3">
+        <FilterDropdown posts={allPostsData} />
+        <BlogCards posts={allPostsData} />
+      </section>
     </main>
   );
 }
--- app/components/cards/blog-cards.tsx
+++ app/components/cards/blog-cards.tsx
@@ -1,38 +1,59 @@
+import Image from "next/image";
+import Link from "next/link";
+import type { PostMetadata } from "@/app/lib/posts";
 import {
   SplitCard,
   SplitCardContent,
   SplitCardHeader,
+  SplitCardThumbnail,
 } from "@/app/components/cards/split-card";
-import Link from "next/link";
-import { PostMetadata } from "@/app/lib/posts";
 
-export default function BlogCards({ posts }: { posts: PostMetadata[] }) {
+interface BlogCardProps {
+  post: PostMetadata;
+}
+
+export function BlogCard({ post }: BlogCardProps) {
   return (
-    <div className="w-full max-w-4xl mx-auto px-4 text-palette-3">
-      <ul className="flex flex-col gap-3">
-        {posts.map((post, index) => (
-          <li key={post.slug}>
-            <Link href={`/blog/${post.slug}`}>
-              <SplitCard className="flex flex-col h-full">
-                <SplitCardContent className="flex-grow">
-                  <SplitCardHeader>
-                    <time className="text-palette-4 text-sm">
-                      {post.publishDate}
-                    </time>
-                    <h1 className="text-left font-bold text-3xl ">
-                      {post.title}
-                    </h1>
-                  </SplitCardHeader>
-                  <p className="text-palette-2 mb-2">{post.description}</p>
-                </SplitCardContent>
-              </SplitCard>
-            </Link>
-            {index < posts.length - 1 && (
-              <div className="border-b border-dotted border-gray-300 my-2"></div>
-            )}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Link href={`/blog/${post.slug}`}>
+      <SplitCard className="flex items-start h-full">
+        <SplitCardThumbnail className="flex-shrink-0 w-32 h-32">
+          <Image
+            src="/assets/shepherd.jpg"
+            alt={`Thumbnail for ${post.title}`}
+            width={300}
+            height={200}
+            className="object-cover w-full h-full rounded-md"
+          />
+        </SplitCardThumbnail>
+        <SplitCardContent className="w-2/3 ml-2">
+          <SplitCardHeader>
+            <time className="text-palette-4 text-sm">{post.publishDate}</time>
+            <h2 className="text-left font-bold text-2xl">{post.title}</h2>
+          </SplitCardHeader>
+          <p className="text-palette-2 text-sm line-clamp-3">
+            {post.description}
+          </p>
+        </SplitCardContent>
+      </SplitCard>
+    </Link>
+  );
+}
+
+interface BlogCardsProps {
+  posts: PostMetadata[];
+}
+
+export default function BlogCards({ posts }: BlogCardsProps) {
+  return (
+    <ul className="flex flex-col gap-3" role="list">
+      {posts.map((post, index) => (
+        <li key={post.slug}>
+          <BlogCard post={post} />
+          {index < posts.length - 1 && (
+            <div className="border-b border-dotted border-gray-300 mt-2.5"></div>
+          )}{" "}
+        </li>
+      ))}
+    </ul>
   );
 }
