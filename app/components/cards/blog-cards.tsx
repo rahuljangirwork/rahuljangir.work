e8d33bd8baf8fb1e927b6c84749df5c06f1520e0
Isai Sanzy <isai10sanchez@gmail.com>
2025-02-24 13:05:06 -0700

    added thumbnails to blog cards using project card's thumbnail component

--- app/components/cards/blog-cards.tsx
+++ app/components/cards/blog-cards.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import type { PostMetadata } from "@/app/lib/posts";
 import {
@@ -7,6 +6,7 @@ import {
   SplitCardHeader,
   SplitCardThumbnail,
 } from "@/app/components/cards/split-card";
+import { ProjectMedia } from "./project-cards";
 
 interface BlogCardProps {
   post: PostMetadata;
@@ -14,28 +14,22 @@ interface BlogCardProps {
 
 export function BlogCard({ post }: BlogCardProps) {
   return (
-    <Link href={`/blog/${post.slug}`}>
-      <SplitCard className="flex items-start h-full">
-        <SplitCardThumbnail className="flex-shrink-0 w-32 h-32">
-          <Image
-            src="/assets/shepherd.jpg"
-            alt={`Thumbnail for ${post.title}`}
-            width={300}
-            height={200}
-            className="object-cover w-full h-full rounded-md"
-          />
-        </SplitCardThumbnail>
-        <SplitCardContent className="w-2/3 ml-2">
-          <SplitCardHeader>
+    <SplitCard className="flex items-start h-full">
+      <SplitCardThumbnail className="flex-shrink-0 aspect-video w-1/3">
+        <ProjectMedia src={post.src} className="rounded-md overflow-hidden" />
+      </SplitCardThumbnail>
+      <Link href={`/blog/${post.slug}`}>
+        <SplitCardContent className="w-full ml-2">
+          <SplitCardHeader className="mb-0.5">
             <time className="text-palette-4 text-sm">{post.publishDate}</time>
-            <h2 className="text-left font-bold text-2xl">{post.title}</h2>
+            <h2 className="text-left font-bold text-3xl">{post.title}</h2>
           </SplitCardHeader>
-          <p className="text-palette-2 text-sm line-clamp-3">
+          <p className="w-4/5 text-palette-2 text-sm line-clamp-2">
             {post.description}
           </p>
         </SplitCardContent>
-      </SplitCard>
-    </Link>
+      </Link>
+    </SplitCard>
   );
 }
 
@@ -45,12 +39,12 @@ interface BlogCardsProps {
 
 export default function BlogCards({ posts }: BlogCardsProps) {
   return (
-    <ul className="flex flex-col gap-3" role="list">
+    <ul className="flex flex-col gap-4" role="list">
       {posts.map((post, index) => (
         <li key={post.slug}>
           <BlogCard post={post} />
           {index < posts.length - 1 && (
-            <div className="border-b border-dotted border-gray-300 mt-2.5"></div>
+            <div className="border-b border-dotted border-gray-300 mt-3"></div>
           )}{" "}
         </li>
       ))}
