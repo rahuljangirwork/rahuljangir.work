64a8d71778f82797bf0c0c337895b146a43841ae
Isai Sanzy <isai10sanchez@gmail.com>
2025-03-13 12:43:22 -0600

    much better styling

--- app/components/cards/blog-cards.tsx
+++ app/components/cards/blog-cards.tsx
@@ -1,53 +1,106 @@
+"use client";
+
 import Link from "next/link";
-import type { PostMetadata } from "@/app/lib/posts";
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import {
   SplitCard,
   SplitCardContent,
   SplitCardHeader,
   SplitCardThumbnail,
 } from "@/app/components/cards/split-card";
-import { ProjectMedia } from "./project-cards";
+import Thumbnail from "@/app/components/media/thumbnail";
+import { Badge } from "@/app/components/ui/badge";
+import { PostMetadata } from "@/app/lib/types";
+import { cn } from "@/app/lib/utils";
 
-interface BlogCardProps {
-  post: PostMetadata;
-}
-
-export function BlogCard({ post }: BlogCardProps) {
+function BlogCard({ post }: { post: PostMetadata }) {
   return (
-    <SplitCard className="flex items-start h-full">
-      <SplitCardThumbnail className="flex-shrink-0 aspect-video w-1/3">
-        <ProjectMedia src={post.src} className="rounded-md overflow-hidden" />
-      </SplitCardThumbnail>
-      <Link href={`/blog/${post.slug}`}>
-        <SplitCardContent className="w-full ml-2">
-          <SplitCardHeader className="mb-0.5">
-            <time className="text-palette-4 text-sm">{post.publishDate}</time>
-            <h2 className="text-left font-bold text-3xl">{post.title}</h2>
-          </SplitCardHeader>
-          <p className="w-4/5 text-palette-2 text-sm line-clamp-2">
-            {post.description}
-          </p>
-        </SplitCardContent>
-      </Link>
-    </SplitCard>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+      className="group rounded-md bg-palette-3/5 backdrop-blur-sm p-4"
+    >
+      <SplitCard className="flex flex-col sm:flex-row gap-4 text-palette-3">
+        <SplitCardThumbnail className="w-full sm:w-2/5 flex-shrink-0 border border-palette-1 rounded-md overflow-hidden">
+          <Thumbnail src={post.src} className="h-full" />
+        </SplitCardThumbnail>
+        <Link href={`/blog/${post.slug}`}>
+          <SplitCardContent className="w-full flex flex-col">
+            <SplitCardHeader className="mb-0.5">
+              <time className="text-palette-4 text-sm">{post.publishDate}</time>
+              <h2 className="text-left font-bold text-3xl">{post.title}</h2>
+            </SplitCardHeader>
+            <p className="w-4/5 text-palette-2 text-sm line-clamp-2">
+              {post.description}
+            </p>
+            <div className="flex flex-wrap gap-2 mt-4">
+              {post.technologies?.map((tag) => (
+                <Badge
+                  key={tag}
+                  className="px-2 py-0 text-palette-2 text-[10px] bg-white/10 shadow-sm hover:bg-primary"
+                >
+                  {tag}
+                </Badge>
+              ))}
+            </div>
+          </SplitCardContent>
+        </Link>
+      </SplitCard>
+    </motion.div>
   );
 }
 
-interface BlogCardsProps {
-  posts: PostMetadata[];
-}
+export default function BlogCards({ posts }: { posts: PostMetadata[] }) {
+  const [activeTab, setActiveTab] = useState<PostMetadata["type"]>("project");
 
-export default function BlogCards({ posts }: BlogCardsProps) {
+  const filteredPosts = posts.filter((post) => post.type === activeTab);
+
   return (
-    <ul className="flex flex-col gap-4" role="list">
-      {posts.map((post, index) => (
-        <li key={post.slug}>
-          <BlogCard post={post} />
-          {index < posts.length - 1 && (
-            <div className="border-b border-dotted border-gray-300 mt-3"></div>
-          )}{" "}
-        </li>
-      ))}
-    </ul>
+    <>
+      <div className="flex space-x-2 mb-6 text-sm mt-1 text-palette-3">
+        <button
+          className={cn(
+            "w-20 rounded-full py-1 px-2 border bg-palette-3/10 backdrop-blur-sm",
+            activeTab === "blog" ? "border-palette-4" : "border-palette-1",
+          )}
+          onClick={() => setActiveTab("blog")}
+        >
+          Blog
+        </button>
+        <button
+          className={cn(
+            "w-24 rounded-full py-1 px-2 border bg-palette-3/10 backdrop-blur-sm",
+            activeTab === "project" ? "border-palette-4" : "border-palette-3",
+          )}
+          onClick={() => setActiveTab("project")}
+        >
+          Projects
+        </button>
+      </div>
+      <ul className="flex flex-col gap-4" role="list">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={activeTab}
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{
+              type: "spring",
+              stiffness: 300,
+              damping: 30,
+            }}
+            className="space-y-6"
+          >
+            {filteredPosts.map((post) => (
+              <li key={post.slug}>
+                <BlogCard post={post} />
+              </li>
+            ))}
+          </motion.div>
+        </AnimatePresence>
+      </ul>
+    </>
   );
 }
