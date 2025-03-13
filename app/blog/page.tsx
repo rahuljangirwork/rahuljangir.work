c949e236a38ef2e2fe74c4e7930962ebde65834f
Isai Sanzy <isai10sanchez@gmail.com>
2025-03-13 11:59:40 -0600

    new type in type.ts file has been imported

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,6 @@
 import { getSortedPostsMetaData } from "@/app/lib/posts";
 import BlogCards from "@/app/components/cards/blog-cards";
-import type { PostMetadata } from "@/app/lib/posts";
-import EmailToast from "@/app/components/ui/email-toast";
-import { MailPlus } from "lucide-react";
-import FilterDropdown from "@/app/components/navigation/filter-dropdown";
+import type { PostMetadata } from "@/app/lib/types";
 
 export default async function Blog() {
   const allPostsData: PostMetadata[] = await getSortedPostsMetaData();
@@ -18,15 +15,6 @@ export default async function Blog() {
           Personal insights on small worlds within engineering and my life
           <span className="text-palette-4">.</span>
         </p>
-        {/*
-        <p className="text-palette-2 hover:text-palette-4 text-sm flex items-center mt-2 transition-all duration-300 ease-in-out hover:cursor-pointer">
-          Shoot me an email
-          <EmailToast iconSize={0}>
-            <MailPlus className="w-4 h-4" />
-          </EmailToast>
-        </p>
-        */}
-        {/*<FilterDropdown posts={allPostsData} />*/}
       </section>
 
       <section className="w-full mx-auto md:w-3/4">
