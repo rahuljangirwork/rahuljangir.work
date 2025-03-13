04b231bb517a5734cfe803c36aecd30feefc4fb2
Isai Sanzy <isai10sanchez@gmail.com>
2025-03-13 12:43:14 -0600

    small gap increase

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,7 +6,7 @@ export default async function Blog() {
   const allPostsData: PostMetadata[] = await getSortedPostsMetaData();
 
   return (
-    <main className="mt-8 flex flex-col md:flex-row md:items-start w-full max-w-4xl gap-2 px-4 lg:px-0 mx-auto text-palette-3 min-h-[70vh]">
+    <main className="mt-8 flex flex-col md:flex-row md:items-start w-full max-w-4xl gap-8 px-4 lg:px-0 mx-auto text-palette-3 min-h-screen">
       <section className="pb-2 text-palette-2 w-full md:w-1/4 flex flex-col items-start gap-1">
         <h1 className="text-4xl font-extrabold">
           Blog Posts<span className="text-palette-4">.</span>
