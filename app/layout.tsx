8417219e48949afde6f8d905fd4d55eeaf340768
Isai Sanzy <isai10sanchez@gmail.com>
2025-04-09 12:28:47 -0600

    metadata has been included to layout.tsx file now

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
     description: "A portfolio of experiences and projects",
     images: [
       {
-        url: "/assets/mebw.JPG",
+        url: "https://www.isais.dev/_next/image?url=%2Fassets%2Fmebw.JPG&w=3840&q=75",
         width: 1200,
         height: 630,
         alt: "Black and White mirror film selfie",
@@ -45,7 +45,9 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Isai - Mechanical Engineer & Software Developer",
     description: "An engineer's portfolio of experiences and projects",
-    images: ["/assets/mebw.JPG"],
+    images: [
+      "https://www.isais.dev/_next/image?url=%2Fassets%2Fmebw.JPG&w=3840&q=75",
+    ],
   },
   icons: {
     icon: "/favicon.ico",
