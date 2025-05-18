de9d45854459f45e12d5dae2ab42aa9b87f73ff5
Isaito <isaisanchezcc@gmail.com>
2025-05-18 14:22:54 -0600

    removed top margin from footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -2,7 +2,7 @@ import { Mail } from "react-feather";
 
 export default function Footer() {
   return (
-    <footer className="bg-palette-2/10 backdrop-blur-md mt-8 p-2 z-20 relative">
+    <footer className="bg-palette-2/10 backdrop-blur-md p-2 z-20 relative">
       <div className="flex justify-between items-center text-palette-2/70">
         <div className="w-full md:w-1/3">
           <p className="text-sm">© 2024 Isai Sanchez. All rights reserved.</p>
