b5e9e47f8c1db10a4b98b84f8cdef2566f744f49
Isaito <isaisanchezcc@gmail.com>
2025-05-18 14:23:23 -0600

    new changes to gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,4 +1,5 @@
 import { getAllGalleryImages } from "@/app/lib/gallery";
+import Link from "next/link";
 import Image from "next/image";
 
 type ImageData = {
@@ -12,8 +13,17 @@ export default async function GalleryPage() {
   const images: ImageData[] = await getAllGalleryImages();
 
   return (
-    <>
-      <div className="mt-12 mx-auto grid max-w-[1000px] grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
+    <div className="min-h-screen bg-gray-100 flex pt-12 px-8 gap-8">
+      <div className="h-72 w-1/5 flex flex-col justify-between">
+        <div className="text-palette-1">
+          <h1 className="text-5xl font-bold">Isai Sanchez</h1>
+          <p>my life in film.</p>
+        </div>
+        <Link href="/" className="text-palette-4">
+          {`< back home.`}
+        </Link>
+      </div>
+      <div className="h-full w-full grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
         {images.map((image, idx) => {
           const aspectRatio = image.height / image.width;
           const spanRows = Math.ceil(aspectRatio * 10);
@@ -38,6 +48,6 @@ export default async function GalleryPage() {
           );
         })}
       </div>
-    </>
+    </div>
   );
 }
