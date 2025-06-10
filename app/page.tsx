9a3f5b2265f6c1545de146b374b9b50d2ad610c9
Isaito <isaisanchezcc@gmail.com>
2025-06-10 13:26:28 -0600

    now a server component, fetches post metadata and sends to post-cards component as prop

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,18 @@
-import {
-  SplitCard,
-  SplitCardContent,
-  SplitCardHeader,
-  SplitCardBody,
-} from "@/app/components/cards/split-card";
-import ProjectCards from "@/app/components/cards/project-cards";
+import PostCards from "./components/cards/post-cards";
 import Hero from "./components/hero";
 import About from "./components/about";
 import BentoGrid from "./components/cards/bento-grid";
+import { getSortedPostsMetaData } from "@/app/lib/posts";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getSortedPostsMetaData();
+
   return (
     <div className="max-w-3xl mx-auto px-4 md:px-0">
       <Hero />
       <BentoGrid />
       <section id="projects" className="scroll-mt-24 text-palette-2">
-        <SplitCard>
-          <SplitCardContent side="all">
-            <SplitCardHeader>
-              <h1 className="text-left font-bold text-3xl">
-                Projects<span className="text-palette-4">.</span>
-              </h1>
-            </SplitCardHeader>
-            <SplitCardBody>
-              <p className="text-md text-left">
-                Here&apos;s a little bit of what I&apos;ve been working on.
-              </p>
-            </SplitCardBody>
-          </SplitCardContent>
-        </SplitCard>
-        <ProjectCards />
+        <PostCards posts={posts} />
       </section>
       <About />
     </div>
