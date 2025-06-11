de824de905c19324525da81e22040edeb219379c
Isaito <isaisanchezcc@gmail.com>
2025-06-11 12:34:32 -0600

    small styling and responsive changes

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -11,8 +11,8 @@ export default async function Blog() {
   const allPostsData: PostMetadata[] = await getSortedPostsMetaData();
 
   return (
-    <main className="mt-8 flex flex-col md:flex-row md:items-start w-full max-w-4xl gap-8 px-4 lg:px-0 mx-auto text-palette-3 min-h-screen mb-8">
-      <section className="pb-2 text-palette-2 w-full md:w-1/4 flex flex-col items-start gap-1">
+    <main className="mt-8 flex flex-col lg:flex-row md:items-start w-full max-w-4xl lg:gap-8 px-4 lg:px-0 mx-auto text-palette-3 min-h-screen mb-8">
+      <section className="flex flex-col items-start gap-2 pb-6 lg:pb-0 text-palette-2 w-full lg:w-1/4">
         <h1 className="text-4xl font-extrabold">
           Blog Posts<span className="text-palette-4">.</span>
         </h1>
@@ -22,7 +22,7 @@ export default async function Blog() {
         </p>
       </section>
 
-      <section className="w-full mx-auto md:w-3/4">
+      <section className="w-full mx-auto lg:w-3/4">
         <BlogCards posts={allPostsData} />
       </section>
     </main>
